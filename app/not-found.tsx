7283c8d2a35ba452a666ca57f05bee759fb98603
xiaomuahuaGG <xiaomuahua666@gmail.com>
2025-06-09 12:46:06 +0800

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ export default function NotFound() {
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-gray-900 mb-4">403</h1>
-        <h2 className="text-2xl font-semibold text-gray-700 mb-4">傻逼滚开！</h2>
+        <h2 className="text-2xl font-semibold text-gray-700 mb-4">傻逼滚开!</h2>
         <h3 className="text-1g font-semibold text-gray-500 mb-4">Asshole, Fuck off!</h3>
       </div>
     </div>
