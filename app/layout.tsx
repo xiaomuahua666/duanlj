f57a8b44c96dc4d97a7c68ea273dfaf50486d8c6
Evan Huang <evan.huang000@proton.me>
2025-07-04 09:34:53 +0800

    fix analytics bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
-      <Analytics/>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
